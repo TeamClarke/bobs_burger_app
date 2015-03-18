b939874567795d45150c92b22a9cf26ab0fe9479
Susan <susan@marpy.net>
2015-03-18 18:31:24 -0400

    grunt build with edited gruntfile

--- test/spec/test.spec.js
+++
@@ -1,6 +0,0 @@
-// describe('Router', function () {
-//     it("provides the 'router' object", function () {
-//       // Expect exists and is an object.
-//       expect(router).to.be.an("object");
-//     })
-// });
